4923d3ae4ed4567af87317ce99795ee3f8f3894f
Arash Parsa <axparsa@gmail.com>
2016-10-24 14:27:47 -0400

    Update bookiecmds.py

--- bookiecmds.py
+++ bookiecmds.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 import sopel.module, from sopel.module import example, import collections, import sys, import time, from sopel.tools import iteritems, import sopel.loader, import sopel.module, import subprocess, import re, import json
 
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
 ##Here we check if the dictionary exists and has something in it.  If not, we create an empty dictionary.#######
 try:
     f = open('/home/sopel/.sopel/modules/dictionary', 'r')
